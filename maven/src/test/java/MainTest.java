85e17b1aa81c6d060d860571d023dc4cd4dc8f22
nashaofu <diaocheng@outlook.com>
2020-01-28 14:40:15 +0800

    修改maven

--- maven/src/test/java/MainTest.java
+++ maven/src/test/java/MainTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
   @Test
   void testFact() {
-    assertEquals(1, Factorial.fact(1));
-    assertEquals(2, Factorial.fact(2));
-    assertEquals(6, Factorial.fact(3));
-    assertEquals(3628800, Factorial.fact(10));
-    assertEquals(2432902008176640000L, Factorial.fact(20));
+    assertEquals(1, Main.fact(1));
+    assertEquals(2, Main.fact(2));
+    assertEquals(6, Main.fact(3));
+    assertEquals(3628800, Main.fact(10));
+    assertEquals(2432902008176640000L, Main.fact(20));
   }
 }
